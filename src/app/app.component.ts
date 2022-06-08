4a926025588fa0a3003c7f432707fd48fbc53fd6
Arthur Ming <guobin.ming@pwc.com>
2022-06-08 23:24:59 +0800

    fix: bootstrapping an applicaion using a standadlone component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { ButtonListComponent } from './button-list/button-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [
+   // CommonModule,
+    // BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    ButtonListComponent,
+  ],
+  standalone: true,
 })
 export class AppComponent {
   title = 'angular-standalone-components-app';
